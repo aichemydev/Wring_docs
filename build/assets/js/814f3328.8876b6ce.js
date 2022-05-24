d334ee270106fd25797a959e8b01a314404f3b93
TeutaUkshini <44062876+TeutaUkshini@users.noreply.github.com>
2022-05-25 01:09:53 +0200

    update fix build

---
+++ build/assets/js/814f3328.8876b6ce.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunktest_gold=self.webpackChunktest_gold||[]).push([[535],{5641:function(t){t.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Introducing Quikly","permalink":"/Quikly_docs/blog/introducing-Quikly"}]}')}}]);
