7982a8a31736afe01104bb688aa29ad6e8db4403
TeutaUkshini <44062876+TeutaUkshini@users.noreply.github.com>
2022-06-02 02:22:48 +0200

    Deploy website - based on a8dcb5d52ae2c5d43f3f39c9e23ad9d20c6a38a5

--- build/assets/js/814f3328.8876b6ce.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunktest_gold=self.webpackChunktest_gold||[]).push([[535],{5641:function(t){t.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Introducing Quikly","permalink":"/Quikly_docs/blog/introducing-Quikly"}]}')}}]);
--- build/assets/js/83933ffc.cffd8018.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunktest_gold=self.webpackChunktest_gold||[]).push([[327],{2559:function(e){e.exports=JSON.parse('{"permalink":"/Quikly_docs/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
