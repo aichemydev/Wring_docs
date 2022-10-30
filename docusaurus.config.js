00ce585040548d95bae51974c2670d75dda42b87
TeutaUkshini <44062876+TeutaUkshini@users.noreply.github.com>
2022-10-30 20:30:44 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,7 +55,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/aichemydev/Wring-issues',
+            href: 'https://github.com/aichemydev/Wring_docs/issues',
             label: 'GitHub',
             position: 'right',
           },
